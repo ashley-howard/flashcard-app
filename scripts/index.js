3e1884685f059d25ad292e70ed3bbab22e237f74
noteconvert <me@ashley.how>
2019-05-26 22:16:01 +0200

    finish some functionality

--- scripts/index.js
+++ scripts/index.js
@@ -13,64 +13,86 @@ const btnGroupDifficulty = document.getElementById("buttons-difficulty");
 const btnEasy = document.getElementById("btn-easy");
 const btnHard = document.getElementById("btn-hard");
 const btnIncorrect = document.getElementById("btn-incorrect");
+const textInfo = document.getElementById("text-info");
+
+var easyCount = 0;
+var hardCount = 0;
+var incorrectCount = 0;
+
+var arrayNum1 = 0;
+var arrayNum2 = 0;
+
 
 function startGame() {
+    arrayNum1 = 0;
+    arrayNum2 = 0;
+    easyCount = 0;
+    hardCount = 0;
+    incorrectCount = 0;
     btnGroupStart.style.display = "none";
+    flashcardText.style.fontSize = "xx-large";
     btnGroupDifficulty.style.display = "block";
-
-    var num1 = 0;
-    var num2 = 0;
+    textInfo.innerHTML = 'Click on the card to turn it over';
 
     // Load first item from array
-    flashcardText.innerHTML = flashcardArr[num1][num2];
-
-    // clicking on flashcard shows other side
-    flashcard.addEventListener("click", flipCard);
-
-    function flipCard() {
-
-        // if side 1 is showing, change to side 2
-        if (num2 === 0) {
-            num2 = 1;
-            flashcardText.innerHTML = flashcardArr[num1][num2];
-        }
-        else if (num2 === 1) {
-            num2 = 0;
-            flashcardText.innerHTML = flashcardArr[num1][num2];
-        }
-
-
-        // if the game hasn't started, don't do anything
-
-        // if shows side 1, flip to side 2
-        //card
-        console.log("hey")
-
-
-        // if shows side 2, flip to side 1
-
-        // reset to side 1 when clicking on btnGroupDifficulty
-
-    }
-
-
+    flashcardText.innerHTML = flashcardArr[arrayNum1][arrayNum2];
 }
 
+// clicking on flashcard shows other side
+flashcard.addEventListener("click", flipCard);
 
+function flipCard() {
+    if ((arrayNum1 + 1) !== flashcardArr.length) {
+        // if side 1 is showing, change to side 2
+        if (arrayNum2 === 0) {
+            arrayNum2 = 1;
+            flashcardText.innerHTML = flashcardArr[arrayNum1][arrayNum2];
+            console.log(`flashcardArr[${arrayNum1}][${arrayNum2}]`);
+        }
+        // vice versa
+        else {
+            arrayNum2 = 0;
+            flashcardText.innerHTML = flashcardArr[arrayNum1][arrayNum2];
+            console.log(`flashcardArr[${arrayNum1}][${arrayNum2}]`);
+        }
+    }
+    else { }
+}
 
+function nextCard() {
+    // pull next card only if arrayNum1 is less than the length of the Array
+    if ((arrayNum1 + 1) !== flashcardArr.length) {
+        arrayNum1 += 1;
+        arrayNum2 = 0;
+        flashcardText.innerHTML = flashcardArr[arrayNum1][arrayNum2];
+    }
+    // otherwise finish, show results, remove buttons-difficulty
+    else {
+        flashcardText.style.fontSize = "medium";
+        flashcardText.innerHTML =
+            "You have finished for today." + "<br>" +
+            "Easy cards: " + easyCount + "<br>" +
+            "Hard cards: " + hardCount + "<br>" +
+            "Incorrect cards: " + incorrectCount;
+
+        btnGroupStart.style.display = "block";
+        btnGroupDifficulty.style.display = "none";
+        btnStart.innerHTML = 'Study again';
+    }
+}
 
 function btn(difficulty) {
     if (difficulty === 'easy') {
-
+        easyCount += 1;
     }
 
     else if (difficulty === 'hard') {
-
+        hardCount += 1;
     }
 
     // incorrect
     else {
-
+        incorrectCount += 1;
     }
-
+    nextCard();
 }
