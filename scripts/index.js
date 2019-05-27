7ef76c68df213dbb8fea616272a575f92b9a9824
noteconvert <me@ashley.how>
2019-05-27 23:27:21 +0200

    user can add custom cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,4 @@
-var flashcardArr = [
-    ["Bello", "Nice"],
-    ["Casa", "House"],
-    ["Ragazza", "Girl"],
-    ["Donna", "Woman"],
-    ["Uomo", "Man"]
-];
+var flashcardArr = localStorage.getItem('flashcards') ? JSON.parse(localStorage.getItem('flashcards')) : [];
 
 var arrEasy = [];
 var arrHard = [];
@@ -16,16 +10,24 @@ function concatArrays() {
 }
 
 const flashcard = document.getElementById("flashcard");
+const flashcardAdd1 = document.getElementById("flashcard-add-1");
+const flashcardAdd2 = document.getElementById("flashcard-add-2");
+const addInfo = document.getElementById("addInfo");
+const settingsInfo = document.getElementById("settingsInfo");
 const flashcardText = document.getElementById("flashcard-text");
 const btnGroupStart = document.getElementById("buttons-start");
+const btnAdd = document.getElementById("btn-add");
+const btnFinishAdd = document.getElementById("btn-add-finish");
 const btnStart = document.getElementById("btn-start");
 const btnGroupDifficulty = document.getElementById("buttons-difficulty");
 const btnEasy = document.getElementById("btn-easy");
 const btnHard = document.getElementById("btn-hard");
 const btnIncorrect = document.getElementById("btn-incorrect");
 const textInfo = document.getElementById("text-info");
-
 const btnSettings = document.getElementById("btn-settings");
+
+const screenIntro = document.getElementById("screenIntro");
+const screenAdd = document.getElementById("screenAdd");
 const screenGame = document.getElementById("screenGame");
 const screenSettings = document.getElementById("screenSettings");
 
@@ -35,6 +37,60 @@ var incorrectCount = 0;
 
 var arrayNum1 = flashcardArr.length + 1; // to prevent 'unwanted' clicks
 var arrayNum2 = 0;
+
+if (flashcardArr.length > 0 ) {
+    changeScreen('game');
+}
+else {
+    changeScreen('intro');
+}
+
+function addCard() {
+    if (flashcardAdd1.value != '' && flashcardAdd2.value != '') {
+        flashcardArr.push([flashcardAdd1.value, flashcardAdd2.value])
+        localStorage.setItem('flashcards', JSON.stringify(flashcardArr));
+        flashcardAdd1.value = '';
+        flashcardAdd2.value = '';
+        flashcardAdd1.focus();
+    }
+    else {
+        addInfo.innerHTML = "Please write on both sides before clicking add";
+        setTimeout(function () {
+            addInfo.innerHTML = "";
+        }, 2000);
+    }
+
+}
+
+screenAdd.onload = flashcardAdd1.focus();
+
+// Enter key front card
+flashcardAdd1.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        flashcardAdd2.focus();
+    }
+});
+
+// Enter key back card
+flashcardAdd2.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        btnAdd.click();
+    }
+});
+
+function finishAddCard() {
+    if (flashcardArr.length > 0) {
+        changeScreen('game');
+    }
+    else {
+        addInfo.innerHTML = "Please add a card before finishing";
+        setTimeout(function () {
+            addInfo.innerHTML = "";
+        }, 2000);
+    }
+}
 
 
 function startGame() {
@@ -173,14 +229,39 @@ function infoMessage(type) {
 }
 
 function changeScreen(screen) {
-    if (screen === 'settings') {
-        btnSettings.setAttribute("onclick", `changeScreen('game')`);
+    if (screen === 'intro') {
+        screenIntro.style.display = 'block';
+        screenAdd.style.display = 'none';
         screenGame.style.display = 'none';
-        screenSettings.style.display = 'block';
+        screenSettings.style.display = 'none';
     }
-    else {
+
+    else if (screen === 'add') {
+        screenIntro.style.display = 'none';
+        screenAdd.style.display = 'block';
+        screenGame.style.display = 'none';
+        screenSettings.style.display = 'none';
+        //flashcardAdd1.focus();
+    }
+
+    else if (screen === 'game') {
         btnSettings.setAttribute("onclick", `changeScreen('settings')`);
+        screenIntro.style.display = 'none';
+        screenAdd.style.display = 'none';
         screenGame.style.display = 'block';
         screenSettings.style.display = 'none';
     }
+
+    else if (screen === 'settings') {
+        btnSettings.setAttribute("onclick", `changeScreen('game')`);
+        screenIntro.style.display = 'none';
+        screenAdd.style.display = 'none';
+        screenGame.style.display = 'none';
+        screenSettings.style.display = 'block';
+    }
+}
+
+function clearData(){
+    localStorage.clear();
+    settingsInfo.innerHTML = "Please refresh to see changes"
 }
